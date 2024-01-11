514f568f715659ef6c736982a75d7a9296d7d496
residentsilve <testoftest.fornara@gmail.com>
2024-01-11 14:02:29 +0900

    CSS調整

--- resources/views/comparisons/top.blade.php
+++ resources/views/comparisons/top.blade.php
@@ -11,23 +11,25 @@ dd($items);
         <input type="text" name="keyword" placeholder="検索キーワードを入力">
         <button type="submit">検索</button>
     </form>
-    
+</div>
+
+    <div class="contents_all">
     @if(isset($items))
         <!-- 検索結果表示などを追加 -->
         @foreach($items as $item)
-            <div>
-                <p>{{ $item['title'] }}</p>
+            <div class="content" >
+                <p >{{ $item['title'] }}</p>
                 <p>{{ $item['price'] }}円</p>
                 @if(isset($item['img']))
                 <img src="{{ $item['img'] }}" alt="{{ $item['title'] }}">
             @else
                 <p>画像なし</p>
             @endif
-                <!-- 他の情報も表示 -->
+            <p>{{ $item['shop'] }}</p>
+                <!-- 他の情報も表示可能 -->
             </div>
         @endforeach
     @endif
-
 
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,78 +1,92 @@
 <html>
 <head>
     <title>@yield('title')</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-<style>
-    body {font-size: 16pt; 
-        color: #999; 
-        margin: 0 auto; 
-        width: 1000px;
-        
+
+    <style>
+      body {
+            margin: 0 auto; 
+            width: 1300px;
+        }
+      /* 
+        h1 {font-size: 50pt ; 
+            text-align: left; 
+            color:rgba(0, 0, 255, 0.534);
+            margin: auto auto; 
+            letter-spacing: -4pt;
+        }
+    
+            ul{
+                font-size: 12pt;
+            }
+    
+            hr{
+                margin: 25px 100px ; 
+                border-top: 1px dashed #ddd;
+            }
+    
+            .manutitle {
+                font-size: 14pt; 
+                font-weight: bold;
+                margin: 0px;
+            }
+    
+            .content {
+                margin: 10px;
+            }
+    
+            .footer {
+                text-align: right;
+                font-size: 10pt;
+                margin: 0px;
+                border-bottom: solid 1px #ccc;
+                color: #ccc;
+            }
+            
+            th {
+                background-color: #854be2;
+                color: fff;
+                padding: 5px 10px;
+            }
+    
+            td{
+                border: solid 1px #854be2;
+                color: #999;
+                padding: 5px 10px;
+            }
+            
+            td.id{
+                width: 20px;
+                text-align: center;
+            }
+            
+            input.del{
+                margin-top: 15px;
+            }
+    
+            input.add{
+                margin-left:500px; 
+            } */
+    
+    div.contents_all{
+        display: flex;
+        width: 1200px;
+        flex-wrap:wrap ;
+        justify-content: space-around;
 
     }
-    h1 {font-size: 50pt ; 
-        text-align: left; 
-        color:rgba(0, 0, 255, 0.534);
-        margin: auto auto; 
-        letter-spacing: -4pt;
+    
+    div.content{
+        width: 350px;
+        margin: 10px;   
     }
-        ul{
-            font-size: 12pt;
-        }
-        hr{
-            margin: 25px 100px ; 
-            border-top: 1px dashed #ddd;
-        }
-        .manutitle {
-            font-size: 14pt; 
-            font-weight: bold;
-            margin: 0px;
-        }
-        .content {
-            margin: 10px;
-        }
-        .footer {
-            text-align: right;
-            font-size: 10pt;
-            margin: 0px;
-        border-bottom: solid 1px #ccc;
-        color: #ccc;
-        }
-        th {
-            background-color: #854be2;
-            color: fff;
-            padding: 5px 10px;
-        }
-
-        td{
-            border: solid 1px #854be2;
-            color: #999;
-            padding: 5px 10px;
-        }
-        
-        td.id{
-            width: 20px;
-            text-align: center;
-        }
-        
-        input.del{
-            margin-top: 15px;
-        }
-
-        input.add{
-            margin-left:500px; 
-        }
-
-
-</style>
+    
+    </style>
 </head>
 <body>
     @section('menubar')
-        <div class="content">
-            @yield('content')
-        </div>
-        <div class="footer">
-            @yield('footer')
-        </div>
+    @yield('content')
+    <div class="footer">
+        @yield('footer')
+    </div>
 </body>
 </html>
